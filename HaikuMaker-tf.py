0777fea2de6998c088f8dea9652ebefb2cdfbcde
caux <tiago.caux@gmail.com>
2016-04-01 20:24:05 +0200

    Getting example stuff here

--- HaikuMaker-tf.py
+++ HaikuMaker-tf.py
@@ -1,50 +1,92 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow.models.rnn as rnn
 import time
 from os import listdir
 from os.path import isfile, join
 
-import Network
+import NN_Model
 
+class Config(object):
+    """Small config."""
+    init_scale = 0.1
+    learning_rate = 1.0
+    max_grad_norm = 5
+    num_layers = 2
+    num_steps = 20
+    hidden_size = 200
+    max_epoch = 4
+    max_max_epoch = 13
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 20
+    vocab_size = 10000
+
+
+def run_epoch(session, m, data, eval_op, verbose=False):
+    """Runs the model on the given data."""
+    epoch_size = ((len(data) // m.batch_size) - 1) // m.num_steps
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = m.initial_state.eval()
+    for step, (x, y) in enumerate(reader.ptb_iterator(data, m.batch_size,
+                                                    m.num_steps)):
+    cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+                                 {m.input_data: x,
+                                  m.targets: y,
+                                  m.initial_state: state})
+    costs += cost
+    iters += m.num_steps
+
+    if verbose and step % (epoch_size // 10) == 10:
+        print("%.3f perplexity: %.3f speed: %.0f wps" %
+            (step * 1.0 / epoch_size, np.exp(costs / iters),
+             iters * m.batch_size / (time.time() - start_time)))
+
+    return np.exp(costs / iters)
 
 def main():
     inputSize = 512
     numOfNeurons = 32
 
-    data = readTrainingSet("/Users/caux/Documents/Development/Datasets/Haiku/", inputSize)
+    data, dict, numWords = readTrainingSet("/Users/caux/Documents/Development/Datasets/Haiku/")
 
-    # lstm = rnn.rnn_cell.BasicLSTMCell(numOfNeurons)
-    # state = tf.zeros([inputSize, lstm.state_size])
-    #
-    # loss = 0.0
-    #
-    # for current_batch_of_words in data:
-    #     # The value of state is updated after processing each batch of words.
-    #     output, state = lstm(current_batch_of_words, state)
-    #
-    #     # The LSTM output can be used to make next word predictions
-    #     logits = tf.matmul(output, softmax_w) + softmax_b
-    #     probabilities = tf.nn.softmax(logits)
-    #     loss += loss_function(probabilities, target_words)
+    config = Config()
+
+    with tf.Graph().as_default(), tf.Session() as session:
+        initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                    config.init_scale)
+        with tf.variable_scope("model", reuse=None, initializer=initializer):
+            m = NN_Model.PTBModel(is_training=True, config=config)
+
+        with tf.variable_scope("model", reuse=True, initializer=initializer):
+            mvalid = NN_Model.PTBModel(is_training=False, config=config)
+            mtest = NN_Model.PTBModel(is_training=False, config=eval_config)
+
+        tf.initialize_all_variables().run()
+
+        for i in range(config.max_max_epoch):
+            lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
+            m.assign_lr(session, config.learning_rate * lr_decay)
+
+            print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+            train_perplexity = run_epoch(session, m, train_data, m.train_op,
+                                         verbose=True)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+            valid_perplexity = run_epoch(session, mvalid, valid_data, tf.no_op())
+            print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+        test_perplexity = run_epoch(session, mtest, test_data, tf.no_op())
+        print("Test Perplexity: %.3f" % test_perplexity)
 
 
-
-    # topology = [inputSize, 256, 64, 16, 4]
-    #
-    # weights, biases = Network.createWeightsAndBiases(topology)
-    #
-    # x, y, y_, layers = Network.createTrainingTopology(topology, 2, weights, biases)
-    # cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-    # train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-    #
-    # with tf.Session() as sess:
-    #     sess.run(tf.initialize_all_variables())
-    #     train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-
-def readTrainingSet(path, inputSize):
+def readTrainingSet(path):
     fileList = []
+    allHaikus = []
+    wordDict = dict()
+    numUniqueWords = 0
+
+    wordDict[""] = numUniqueWords   # EOH (End Of Haiku)
 
     for f in listdir(path):
         if ".txt" in f:
@@ -52,25 +94,26 @@ def readTrainingSet(path, inputSize):
             if isfile(filePath):
                 fileList.append(filePath)
 
-    data = np.empty([len(fileList), inputSize])
-
     for indexFile in range(len(fileList)):
         with open(fileList[indexFile], 'r') as haikuFile:
             haikuData = haikuFile.read()
 
             haikuData = haikuData.replace("\r\n", "\n")
             haikuData = haikuData.split("\n\n")[0]
+            splitHaikuData = haikuData.split(" ")
 
-            print haikuData
-            print
-            print
+            # ID all words using a map
+            for word in splitHaikuData:
+                if word != "":
+                    if word not in wordDict:
+                        wordDict[word] = numUniqueWords
+                        numUniqueWords += 1
 
-            for indexChar in range(len(haikuData)):
-                data[indexFile][indexChar] = float(ord(haikuData[indexChar])) / 255.0
+                    allHaikus.append(wordDict[word])
 
-    return data
+            allHaikus.append(wordDict[""])
 
-
+    return allHaikus, wordDict, numUniqueWords
 
 if __name__ == "__main__":
     start_time = time.time()
